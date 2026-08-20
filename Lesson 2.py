5c0d114c6aeb208eee6f069fba932da68cca40c8
mghaddaf <mghaddaf@gmail.com>
2026-08-20 20:02:21 +0100

    Add files via upload

--- Lesson 2.py
+++ Lesson 2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 destroyed_land = cv2.imread(r"Open CV\Images\DESTROYED LAND.jpg", cv2.IMREAD_COLOR)
 night_sky = cv2.imread(r"Open CV\Images\NIGHT SKY.jpg", cv2.IMREAD_COLOR)
 cv2.imshow("Destroyed Land", destroyed_land)
@@ -20,4 +21,39 @@ cv2.waitKey(0)
 night_sky = cv2.imread(r"Open CV\Images\NIGHT SKY.jpg", cv2.IMREAD_COLOR)
 resized = cv2.resize(night_sky, (200, 200)) #RESIZES THE IMAGE
 cv2.imshow("RESIZED", resized)
+cv2.waitKey(0)
+
+# EROSION OF AN IMAGE
+minecraft_menu = cv2.imread(r"Open CV\Images\minecraft menu.png", cv2.IMREAD_COLOR)
+cv2.imshow("minecraft menu", minecraft_menu)
+kernel = np.ones((5, 5), np.uint8)
+image = cv2.erode(minecraft_menu, kernel)
+cv2.imshow("eroded image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#BORDERING AROUND THE IMAGE
+minecraft_menu = cv2.imread(r"Open CV\Images\minecraft menu.png", cv2.IMREAD_COLOR)
+cv2.imshow("minecraft menu", minecraft_menu)
+#SOLID BORDER AROUND THE IMAGE
+solidborder = cv2.copyMakeBorder(minecraft_menu, 20, 20, 20, 20, cv2.BORDER_CONSTANT, value = 1)
+cv2.imshow("solidborder", solidborder)
+cv2.waitKey(0)
+
+#REFLECTIVE BORDER
+reflectiveborder = cv2.copyMakeBorder(minecraft_menu, 20, 20, 20, 20, cv2.BORDER_REFLECT, value = 1)
+cv2.imshow("reflect border", reflectiveborder)
+cv2.waitKey(0)
+
+#BLURRING
+minecraft_person = cv2.imread(r"Open CV\Images\minecraft person.png", cv2.IMREAD_COLOR)
+cv2.imshow("minecraft person", minecraft_person)
+#gaussing blurring
+gaussing_blur = cv2.GaussianBlur(minecraft_person, (11, 11), 0)
+cv2.imshow("gaussing blur", gaussing_blur)
+cv2.waitKey(0)
+
+#MEDIAN BLUR
+median_blur = cv2.medianBlur(minecraft_person, 5)
+cv2.imshow("Median blur", median_blur)
 cv2.waitKey(0)
